034b775f27e8616fdf57ad86d58a06fdd6747368
alvarocabo <alvarocaboac2@gmail.com>
2023-05-26 16:41:23 +0200

    Migration definitions

--- priv/repo/migrations/20230526142317_create_events_table.exs
+++
@@ -1,7 +0,0 @@
-defmodule BetUnfair.Repo.Migrations.CreateEventsTable do
-  use Ecto.Migration
-
-  def change do
-
-  end
-end
--- priv/repo/migrations/20230526142616_create_bets_table.exs
+++ priv/repo/migrations/20230526142616_create_bets_table.exs
@@ -4,8 +4,6 @@ defmodule BetUnfair.Repo.Migrations.CreateBetsTable do
   def change do
     create table(:bets) do
       add :bet_id, :integer
-      add :user_id, references(:users)
-      add :event_id, references(:events)
       add :amount, :float
       add :odds, :float
       add :bet_type, :string
