882c098a90a2e865d766c3371511e474e0231073
alvarocabo <alvarocaboac2@gmail.com>
2023-05-28 16:21:54 +0200

    Changed schemas

--- priv/repo/migrations/20230526143928_create_maket_table.exs
+++ priv/repo/migrations/20230526143928_create_maket_table.exs
@@ -1,14 +1,15 @@
-defmodule BetUnfair.Repo.Migrations.CreateEventsTable do
+defmodule MyApp.Repo.Migrations.CreateMarketTable do
   use Ecto.Migration
 
   def change do
     create table(:market, primary_key: false) do
-      add :market_id, :bigint, primary_key: true
-      add :market_name, :string
+      add :market_name, :string, primary_key: true
       add :market_description, :string
       add :market_date, :utc_datetime
-      add :is_settled, :boolean
-      add :outcome, :string
+      add :status, :string
+      add :settled_result, :boolean
+
+      timestamps()
     end
   end
 end
