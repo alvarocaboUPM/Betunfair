bc9c2315b10a9e87dc4115b0ddbf32175f98f4da
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2023-05-29 12:44:02 +0200

    Added new fields to bet schema

--- priv/repo/migrations/20230527221749_create_bet_table.exs
+++ priv/repo/migrations/20230527221749_create_bet_table.exs
@@ -4,11 +4,13 @@ defmodule BetUnfair.Repo.Migrations.CreateBetsTable do
   def change do
     create table(:bet) do
       add :amount, :float
+      add :remaining_amount, :float
       add :odds, :float
       add :bet_type, :string
       add :is_matched, :boolean
       add :username, references(:user, column: :username, type: :string)
       add :market_name, references(:market, column: :market_name, type: :string)
+      add :status, :string
       timestamps()
     end
   end
